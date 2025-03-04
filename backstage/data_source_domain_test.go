a86f1ef927eaf073810be765f85d866260d23e41
Tomas Dabašinskas <tomas@dabasinskas.net>
2025-03-04 13:42:44 +0200

    fix(backstage): update relation target reference in domain test
    The test was checking for `relations.0.target.name` but the API now returns `relations.0.target_ref` with a fully qualified reference in the format `system:default/artist-engagement-portal`.

--- backstage/data_source_domain_test.go
+++ backstage/data_source_domain_test.go
@@ -18,7 +18,7 @@ func TestAccDataSourceDomain(t *testing.T) {
 					resource.TestCheckResourceAttr("data.backstage_domain.test", "metadata.annotations.backstage.io/source-location",
 						"url:https://github.com/backstage/backstage/tree/master/packages/catalog-model/examples/domains/"),
 					resource.TestCheckResourceAttr("data.backstage_domain.test", "metadata.description", "Everything related to artists"),
-					resource.TestCheckResourceAttr("data.backstage_domain.test", "relations.0.target.name", "artist-engagement-portal"),
+					resource.TestCheckResourceAttr("data.backstage_domain.test", "relations.0.target_ref", "system:default/artist-engagement-portal"),
 					resource.TestCheckResourceAttr("data.backstage_domain.test", "spec.owner", "team-a"),
 				),
 			},
